cea80c5fa05353beb57db11b4f93df4d34dd4c65
Ugwu Hilary <ugwuhilary017@gmail.com>
2024-02-13 08:08:10 +0100

    initial commit

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -41,43 +41,99 @@
 
 
 
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams
-  const id:any = searchParams.get("id")
-  const idAsNumber = parseInt(id)
+// async function getResponse(req: NextRequest): Promise<NextResponse> {
+//   const searchParams = req.nextUrl.searchParams
+//   const id:any = searchParams.get("id")
+//   const idAsNumber = parseInt(id)
 
-  const nextId = idAsNumber + 1;
-  const randomInt = Math.floor(Math.random() * 100);
-  const imageUrlBase = `https://picsum.photos/id/${randomInt}/1146/600`;
+//   const nextId = idAsNumber + 1;
+//   const randomInt = Math.floor(Math.random() * 100);
+//   const imageUrlBase = `https://picsum.photos/id/${randomInt}/1146/600`;
 
-  if(idAsNumber === 7){
-      return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame 7</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://picsum.photos/id/${randomInt}/1146/600" />
-    <meta property="fc:frame:button:1" content="Cosmic Cowboys" />
-    <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Blog post Tutorial" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Video Tutorial" />
-    <meta property="fc:frame:button:3:action" content="post_redirect" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
-  </head></html>`);
-  } else {
-  return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame ${id}</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://picsum.photos/id/${randomInt}/1146/600" />
-    <meta property="fc:frame:button:1" content="Next Page" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
-  </head></html>`);
+//   if(idAsNumber === 7){
+//       return new NextResponse(`<!DOCTYPE html><html><head>
+//     <title>This is frame 7</title>
+//     <meta property="fc:frame" content="vNext" />
+//     <meta property="fc:frame:image" content="https://picsum.photos/id/${randomInt}/1146/600" />
+//     <meta property="fc:frame:button:1" content="Cosmic Cowboys" />
+//     <meta property="fc:frame:button:1:action" content="post_redirect" />
+//     <meta property="fc:frame:button:2" content="Blog post Tutorial" />
+//     <meta property="fc:frame:button:2:action" content="post_redirect" />
+//     <meta property="fc:frame:button:3" content="Video Tutorial" />
+//     <meta property="fc:frame:button:3:action" content="post_redirect" />
+//     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+//   </head></html>`);
+//   } else {
+//   return new NextResponse(`<!DOCTYPE html><html><head>
+//     <title>This is frame ${id}</title>
+//     <meta property="fc:frame" content="vNext" />
+//     <meta property="fc:frame:image" content="https://picsum.photos/id/${randomInt}/1146/600" />
+//     <meta property="fc:frame:button:1" content="Next Page" />
+//     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+//   </head></html>`);
+//   }
+// }
+
+// export async function POST(req: NextRequest): Promise<Response> {
+//   return getResponse(req);
+// }
+
+// export const dynamic = 'force-dynamic';
+
+
+
+
+// ./app/frames/route.ts
+ 
+// export { POST } from "frames.js/next/server";
+
+
+
+
+
+
+
+// handle frame actions
+// ./app/frames/route.ts
+
+import { getFrameHtml, validateFrameMessage, Frame } from "frames.js";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  // Parse and validate the frame message
+  const { isValid, message } = await validateFrameMessage(body);
+  if (!isValid || !message) {
+    return new Response("Invalid message", { status: 400 });
   }
-}
 
-export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req);
-}
+  const randomInt = Math.floor(Math.random() * 100);
+  const imageUrlBase = `https://picsum.photos/seed/${randomInt}`;
 
-export const dynamic = 'force-dynamic';
+  // Use the frame message to build the frame
+  const frame: Frame = {
+    image: `https://picsum.photos/seed/${randomInt}/1146/600`,
+    version: 'vNext',
+    buttons: [
+      {
+        label: `Next (pressed by ${message.data.fid})`,
+        action: 'post'
+      },
+    ],
+    ogImage: `${imageUrlBase}/600`,
+    postUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/frame`,
+  };
+
+
+  const html = getFrameHtml(frame);
+
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+    status: 200,
+  });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,7 @@
 //   return (
 //     <>
 //       <h1>Cosmic Cowboys</h1>
+//       <img src='https://picsum.photos/id/88/1146/600' alt='a pix' />
 //     </>
 //   );
 // }
@@ -35,42 +36,98 @@
 
 
 
-import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
 
-const randomInt = Math.floor(Math.random() * 100);
-const imageUrlBase = `https://picsum.photos/id/${randomInt}/1146/600`;
 
-const frameMetadata = getFrameMetadata({
+
+
+
+// page that renders a frame
+// ./app/page.tsx
+
+import { Frame, getFrameFlattened } from "frames.js";
+import type { Metadata } from "next";
+
+// Declare the frame
+const initialFrame: Frame = {
+  image: "https://picsum.photos/seed/frames.js/1146/600",
+  version: "vNext",
   buttons: [
     {
-      label: 'Begin'
-    }, 
-    {
-      label: 'Start'
-    }
+      label: "Random image",
+      action: 'post'
+    },
   ],
-  image: imageUrlBase,
-  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
-});
+  postUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/frame`,
+};
 
+// Export Next.js metadata
 export const metadata: Metadata = {
-  title: 'Cosmic Cowboys',
-  description: 'A frame telling the story of Cosmic Cowboys',
+  title: "Random Image Frame",
+  description: "This is an example of a simple frame using frames.js",
   openGraph: {
-    title: 'Cosmic Cowboys',
-    description: 'A frame telling the story of Cosmic Cowboys',
-    images: [`https://picsum.photos/id/${randomInt}/1146/600`],
+    images: [
+      {
+        url: "https://picsum.photos/seed/frames.js/600",
+      },
+    ],
   },
-  other: {
-    ...frameMetadata,
-  },
+  other: getFrameFlattened(initialFrame),
 };
 
 export default function Page() {
   return (
     <>
       <h1>Cosmic Cowboys</h1>
+      <img src='https://picsum.photos/id/88/1146/600' alt='a pix' />
     </>
   );
 }
+
+
+
+
+
+
+
+
+
+
+// import { getFrameMetadata } from '@coinbase/onchainkit';
+// import type { Metadata } from 'next';
+
+// const randomInt = Math.floor(Math.random() * 100);
+// const imageUrlBase = `https://picsum.photos/id/${randomInt}/1146/600`;
+
+// const frameMetadata = getFrameMetadata({
+//   buttons: [
+//     {
+//       label: 'Begin'
+//     }, 
+//     {
+//       label: 'Start'
+//     }
+//   ],
+//   image: imageUrlBase,
+//   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+// });
+
+// export const metadata: Metadata = {
+//   title: 'Lary',
+//   description: 'This is a story about Lary',
+//   openGraph: {
+//     title: 'Lary',
+//     description: 'A frame telling the story of Lary',
+//     images: [`https://picsum.photos/id/${randomInt}/1146/600`],
+//   },
+//   other: {
+//     ...frameMetadata,
+//   },
+// };
+
+// export default function Page() {
+//   return (
+//     <>
+//       <h1>Cosmic Cowboys</h1>
+//     </>
+//   );
+// }
